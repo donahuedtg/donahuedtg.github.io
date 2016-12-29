b51b6d3b7c642a3e6e2f6fe66b04a53e0f9c8a6c
min_max_fx <min_max_fx@abv.bg>
2016-12-29 20:52:42 +0200

    add new items

---
+++ Page/footer-main.php
@@ -0,0 +1,3 @@
+<footer>
+	<p><a href="index.php">Design DTG Studio &copy; <?php echo date('Y')?></a></p>
+</footer>
---
+++ Page/header-main.php
@@ -0,0 +1,44 @@
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <ul class="nav navbar-nav navbar-right">
+				<li class="active first"><a href="index.php">DtgStudio</a></li>
+                </li>
+            </div>
+			
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav navbar-right">
+				<li class="active first"><a href="index.php">Начало</a></li>
+
+                <li class="dropdown second">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" 
+                    aria-haspopup="true" aria-expanded="false">Проекти <span class="caret"></span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a href="http://timberlux.bg">Timberlux</a></li>
+                    <li><a href="http://azares.eu">Azares</a></li>
+                    <li><a href="http://markabg.com/index22.php">MarkaBG</a></li>
+                    <li><a href="http://mosaicohispano.com">Mosaico</a></li>
+
+                  </ul>
+                </li>
+
+
+              </ul>
+            </div>
+			
+			<hr>
+          </div>
+        </nav>
+
+      </div>
+    </div>
